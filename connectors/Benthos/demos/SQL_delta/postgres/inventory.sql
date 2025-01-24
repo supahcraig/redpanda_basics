ae6f85bd48ca5dd5a7eb297d9e65f0a766c4f63d
Craig Nelson <34287157+supahcraig@users.noreply.github.com>
2025-01-24 07:47:57 -0600

    Create inventory.sql

---
+++ connectors/Benthos/demos/SQL_delta/postgres/inventory.sql
@@ -0,0 +1,35 @@
+CREATE TABLE inventory.products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(512),
+    weight FLOAT
+);
+
+
+CREATE TABLE inventory.products_on_hand (
+  product_id SERIAL PRIMARY KEY,
+  quantity INT NOT NULL,
+  FOREIGN KEY (product_id) REFERENCES inventory.products(id)
+);
+
+
+CREATE TABLE inventory.customers (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE inventory.orders (
+  id SERIAL PRIMARY KEY,
+  order_date DATE NOT NULL,
+  purchase_date DATE NOT NULL,
+  purchaser INT NOT NULL,
+  quantity INT NOT NULL,
+  product_id INT NOT NULL,
+  FOREIGN KEY (purchaser) REFERENCES inventory.customers(id),
+  FOREIGN KEY (product_id) REFERENCES inventory.products(id)
+);
+
+
