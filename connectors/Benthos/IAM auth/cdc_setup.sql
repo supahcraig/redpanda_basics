f4970ca2b05e02fa751cf2163f78767d28881b9a
Craig Nelson <34287157+supahcraig@users.noreply.github.com>
2026-01-16 01:41:23 -0600

    Update cdc_setup.sql

--- connectors/Benthos/IAM auth/cdc_setup.sql
+++ connectors/Benthos/IAM auth/cdc_setup.sql
@@ -1,1 +1,15 @@
-placeholder
+-- Example replication user that will authenticate via IAM token (no password needed)
+CREATE USER iam_demo_user;
+
+-- Allow IAM token auth for this user
+GRANT rds_iam TO iam_demo_user;
+
+-- Allow logical replication privileges
+GRANT rds_replication TO iam_demo_user;
+
+-- (Optional but common) allow connecting to your DB
+GRANT CONNECT, CREATE ON DATABASE demo_db TO iam_demo_user;
+
+-- Create the publication as the table owner
+CREATE PUBLICATION pglog_stream_rpcn_iam_test FOR TABLE public.iamuser_test;
+
