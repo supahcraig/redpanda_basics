587f1e6a2d61da05c71514851d43e5cc7c650524
Craig Nelson <34287157+supahcraig@users.noreply.github.com>
2026-01-16 11:44:11 -0600

    Update cdc_setup.sql

--- connectors/Benthos/IAM auth/cdc_setup.sql
+++ connectors/Benthos/IAM auth/cdc_setup.sql
@@ -10,6 +10,11 @@ GRANT rds_replication TO iam_demo_user;
 -- (Optional but common) allow connecting to your DB
 GRANT CONNECT, CREATE ON DATABASE demo_db TO iam_demo_user;
 
+-- Create a basic table to replicate
+CREATE TABLE iamuser_test(x int);
+
+INSERT INTO iamuser_test VALUES (1);
+
 -- Create the publication as the table owner
 CREATE PUBLICATION pglog_stream_rpcn_iam_test FOR TABLE public.iamuser_test;
 
